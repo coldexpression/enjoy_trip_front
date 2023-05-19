2c9e48d2bc347458c385493c8923e8ff3c1fe409
손정민 <jungmin0049@gmail.com~>
2023-05-19 15:29:17 +0900

    likepage.js Vuex활용으로 수정

--- src/components/user/likepage.vue
+++ src/components/user/likepage.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <section class="detail_main">
-      <h1 class="list_title">{{ title }}님의 좋아요 페이지</h1>
+      <h1 class="list_title">{{ storeUserId }}님의 좋아요 페이지</h1>
       <ul class="detail_list">
         <li
           class="detail_list_item"
-          v-for="Attraction in FavoriteList"
+          v-for="Attraction in storeBookMarkInfo"
           :key="Attraction.title"
         >
           <router-link
@@ -45,13 +45,7 @@
                 <li @click="log">
                   <font-awesome-icon
                     class="heart_icon"
-                    v-if="Attraction.liked"
                     :icon="['fa-solid', 'fa-heart']"
-                  />
-                  <font-awesome-icon
-                    class="heart_icon"
-                    v-else
-                    :icon="['fa-regular', 'fa-heart']"
                   />
                   {{ Attraction.likeCount | nFormatter(1) }}
                 </li>
@@ -67,34 +61,9 @@
 
 <script>
 import likepage from "@/assets/js/likepage";
-import nFormatter from "@/assets/js/filters";
 export default likepage;
-// import axios from "axios";
-// export default {
-//   data() {
-//     return {
-//       title: "좋아요 페이지",
-//       FavoriteList: []
-//     };
-//   },
-//   created() {
-//     axios
-//       .get("http://localhost:9000/api/attraction/userFavorite", {
-//         params: {
-//           userId: "storeUserId"
-//         }
-//       })
-//       .then(res => {
-//         // console.log(res);
-//         console.log("res.data");
-//         console.log(res.data);
-//         this.FavoriteList = res.data;
-//       });
-//   }
-// };
 </script>
 
 <style scoped>
-/* @import "../../assets/css/detail.css"; */
 @import "../../assets/css/likepage.css";
 </style>
