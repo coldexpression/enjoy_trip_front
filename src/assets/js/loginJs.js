5c083b6e6bed5c0cb9fde28a05ad72cbe1f70987
서찬식 <uni_vers_@naver.com>
2023-05-19 13:39:42 +0900

    # 개발 완료 - 로그인 페이지

--- src/assets/js/loginJs.js
+++ src/assets/js/loginJs.js
@@ -5,11 +5,14 @@ import { mapGetters } from "vuex";
 const userStore = "userStore";
 
 export default {
-  data: () => {
+  data() {
     return {
       id: "",
       pwd: "",
-      loginCheck: false
+      loginCheck: false,
+      errors: [],
+      errorIdCheck: false,
+      errorPwdCheck: false
     };
   },
   computed: {
@@ -19,11 +22,49 @@ export default {
       storeLoginState: "GET_LOGIN_STATE"
     })
   },
+  watch: {
+    id() {
+      this.errors = [];
+      this.errorPwdCheck = false;
+      this.errorIdCheck = false;
+    },
+    pwd() {
+      this.errors = [];
+      this.errorPwdCheck = false;
+      this.errorIdCheck = false;
+    }
+  },
   methods: {
     submitForm(event) {
-      console.log(this);
-      console.log(event.target.id.value);
-      this.$store.dispatch(`${userStore}/AC_USER_LOGIN`, event.target);
+      this.errors = [];
+      if (!this.id) {
+        this.errors.push({ flag: "id", context: "아이디를 입력해 주세요" });
+        this.errorIdCheck = true;
+        return;
+      }
+
+      if (!this.pwd) {
+        this.errors.push({ flag: "pwd", context: "비밀번호를 입력해 주세요" });
+        this.errorPwdCheck = true;
+        return;
+      }
+
+      if (!this.validPassword(this.pwd)) {
+        this.errors.push({
+          flag: "pwd",
+          context: "비밀번호는 영문 + 숫자 8자리 이상 입력해 주세요"
+        });
+        this.errorPwdCheck = true;
+        return;
+      }
+
+      if (this.errors.length == 0) {
+        this.$store.dispatch(`${userStore}/AC_USER_LOGIN`, event.target);
+      }
+    },
+    validPassword(password) {
+      let reg = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+      return reg.test(password);
     }
   }
 };
--- src/components/user/login.vue
+++ src/components/user/login.vue
@@ -6,16 +6,23 @@
           <form v-on:submit.prevent="submitForm" id="login">
             <div class="info">
               <h1>로그인</h1>
+              <b-alert
+                class="login_alert"
+                :show="errorIdCheck || errorPwdCheck"
+                variant="danger"
+              >
+                <a v-for="(error, index) in errors" :key="index">{{
+                  error.context
+                }}</a>
+              </b-alert>
               <p>
-                <input type="hidden" name="action" value="login" />
-              </p>
-              <p>
-                아이디<br /><input
+                아이디<br /><b-form-input
                   type="text"
                   name="id"
                   autofocus
-                  placeholder=" 아이디를 입력하시오"
+                  :state="!errorIdCheck ? null : false"
                   v-model="id"
+                  class="id"
                 />
               </p>
               <p class="remain">
@@ -26,11 +33,12 @@
                 /><label for="remainLogin"> 아이디 저장</label>
               </p>
               <p>
-                비밀번호<br /><input
+                비밀번호<br /><b-form-input
                   type="password"
                   name="pwd"
-                  placeholder=" 비밀번호"
+                  :state="!errorPwdCheck ? null : false"
                   v-model="pwd"
+                  class="pwd"
                 />
               </p>
               <input type="submit" class="submit" value="로그인" />
